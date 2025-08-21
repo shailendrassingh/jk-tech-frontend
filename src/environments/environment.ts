8745b2016d251e437c8e2cd790093221b2c2120f
Shailendra Singh <shailendrassingh90@gmail.com>
2025-08-21 14:15:50 +0530

    Enviornment file

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  authApiUrl: 'http://localhost:3001/auth',
+  userApiUrl: 'http://localhost:3002/users',
+  documentApiUrl: 'http://localhost:3003/documents',
+  ingestionApiUrl: 'http://localhost:3004/ingestion',
+  qaApiUrl: 'http://localhost:3001/qa',
+  featureFlag: true
+};
